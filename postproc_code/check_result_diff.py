dcbb8e7a0230b97be5eebbfefb6a3f2e81ee1a50
Neilen Marais <nmarais@gmail.com>
2011-03-14 11:21:09 +0200

    Port hcyl driver to new fenics based mesh reader

--- postproc_code/check_result_diff.py
+++ postproc_code/check_result_diff.py
@@ -1,18 +1,22 @@
 from __future__ import division
 import pickle, os
 import numpy as N
+import sys
 
-comp_dir = 'hcyl_pin_output'
+sys.path.append('../')
 
-comp_files = ('hybmesh_wg_hcyl_pin_1_h8_o1_dt0.001333.pickle',
+new_res_dir = '../'
+comp_dir = '../hcyl_pin_output'
+
+comp_files = (#'hybmesh_wg_hcyl_pin_1_h8_o1_dt0.001333.pickle',
               'hybmesh_wg_hcyl_pin_1_h8_o2_dt0.000571.pickle',
-              'hybmesh_wg_hcyl_pin_1_h8_o3_dt0.000333.pickle',
+              #'hybmesh_wg_hcyl_pin_1_h8_o3_dt0.000333.pickle',
               )
 comp_keys = ('ts_modeintg1_n', 'ts_modeintg2_n', 'nf1', 'nf2')
 
 for f in comp_files:
     print f
-    res = pickle.load(file(f))
+    res = pickle.load(file(os.path.join(new_res_dir, f)))
     old_res = pickle.load(file(os.path.join(comp_dir, f)))
     for k in comp_keys:
         print k, N.max(N.abs(res[k] - old_res[k]))
--- run_hybmesh_wg_hcyl_pin.py
+++ run_hybmesh_wg_hcyl_pin.py
@@ -13,6 +13,8 @@ import re
 # Local Imports
 #
 from NewCode.Utilities import Struct, partial, close_to_point
+from NewCode import Mesh
+from NewCode.Meshes import CalculateConnectivity, FemmeshReader
 from NewCode import Waveforms
 from NewCode.Runners.HybridWaveGuide2Port import HybridWaveGuide2PortPEC
 from NewCode.GeomGen.Hybrid import NodeInDeadElements, WGSizeCalc
@@ -40,12 +42,19 @@ from NewCode import Consts
 max_dt_div_pow = 0
 FEKO_workspace = 'workspace'
 output_dir = '.'
-orders = (2,3)
+orders = (2,)
 #orders = (1,)
 
     
-eMAGUSImport.init(FEKO_workspace)
-tet_mesh = eMAGUSImport.get_mesh()
+# eMAGUSImport.init(FEKO_workspace)
+# tet_mesh = eMAGUSImport.get_mesh()
+
+meshfile = 'workspace/wg_hcyl_pin_1_h8.femmesh'
+listmesh = CalculateConnectivity.get_all_connectivities(
+    FemmeshReader.get_femmesh_as_listmesh(meshfile))
+tet_mesh = Mesh.Mesh(listmesh)
+
+
 
 print 'Tet_mesh elements: ', len(tet_mesh.elements)
 tetmesh_basename, tetmesh_ext = os.path.splitext(tet_mesh.FemmeshFilename)
