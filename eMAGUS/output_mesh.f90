dbd96e06a6e8b94d425399fd3b1c78fa7f0959bb
Neilen Marais <nmarais@gmail.com>
2011-03-22 20:38:20 +0200

    Re applying eMAGUS changes that somehow got lost in the mergehole

--- eMAGUS/output_mesh.f90
+++ eMAGUS/output_mesh.f90
@@ -22,16 +22,16 @@ CONTAINS
     INTEGER(i4b) :: i
 
     WRITE(mesh_unit, '(A)') 'BLOCK nodes'
-    WRITE(mesh_unit, '(I)') num_nodes
+    WRITE(mesh_unit, '(I0)') num_nodes
     DO i = 1, num_nodes
-       WRITE(mesh_unit, '(I, G, G, G)') i, vertices(i)%coord
+       WRITE(mesh_unit, '(I0, G0, G0, G0)') i, vertices(i)%coord
     END DO
     WRITE(mesh_unit, '(A)') 'ENDBLOCK'
 
     WRITE(mesh_unit, '(A)') 'BLOCK tets'
-    WRITE(mesh_unit, '(I)') num_elements
+    WRITE(mesh_unit, '(I0)') num_elements
     DO i = 1, num_elements
-       WRITE(mesh_unit, '(6I)') i, elements(i)%material, elements(i)%nodes
+       WRITE(mesh_unit, '(6I0)') i, elements(i)%material, elements(i)%nodes
     END DO
     WRITE(mesh_unit, '(A)') 'ENDBLOCK'
 
