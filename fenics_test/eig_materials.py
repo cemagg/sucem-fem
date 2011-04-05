8a290fe2836ca98b9fcce4c4da1a9c71c4ab53b7
Neilen Marais <nmarais@gmail.com>
2011-04-05 18:09:59 +0200

    Adding fenics eigen test with different material regions

---
+++ fenics_test/eig_materials.py
@@ -0,0 +1,114 @@
+from __future__ import division
+
+import sys
+sys.path.append('../')
+import numpy as N
+import os
+import dolfin as dol
+from dolfin import dot, curl, inner, dx
+from scipy.sparse.linalg.eigen.arpack import speigs
+
+from NewCode.Consts import eps0, mu0, c0
+
+import postproc_eigres
+from material_properties import MaterialProperties
+
+# Test for PETSc and SLEPc
+if not dol.has_la_backend("PETSc"):
+    print "DOLFIN has not been configured with PETSc. Exiting."
+    exit()
+
+if not dol.has_slepc():
+    print "DOLFIN has not been configured with SLEPc. Exiting."
+    exit()
+
+# class CavityDims(object): pass
+# cdims = CavityDims()
+# cdims.a, cdims.b, cdims.c = 29,23,19
+
+
+# Define mesh
+# mesh = dol.UnitCube(1,1,1)
+# mesh.coordinates()[:] *= [cdims.a,cdims.b,cdims.c]
+mesh_file = 'lee_mittra92_fig6b.xml'
+#mesh_file = 'albani_bernardi74_fig2VII.xml'
+materials_mesh_file = "%s_physical_region%s" % (os.path.splitext(mesh_file))
+mesh = dol.Mesh(mesh_file)
+materials = {1000:MaterialProperties(eps_r=16/eps0),
+             1001:MaterialProperties(eps_r=1/eps0)}
+eps_vals = dict((k, mat.get_eps()) for k,mat in materials.items())
+
+material_mesh_func = dol.MeshFunction('uint', mesh, materials_mesh_file)
+# Discontinuous function to represent material_properties
+material_func_space = dol.FunctionSpace(mesh, 'DG', 0)
+eps = dol.Function(material_func_space)
+eps.vector()[:] = N.array([eps_vals[int(i)] for i in material_mesh_func.array()])
+
+
+# Define function space
+order = 4
+V = dol.FunctionSpace(mesh, "Nedelec 1st kind H(curl)", order)
+
+# Define basis and bilinear form
+u = dol.TrialFunction(V)
+v = dol.TestFunction(V)
+
+m = eps*inner(v, u)*dx                  # Mass form
+# m = inner(v, u)*dx                  # Mass form
+s = dot(curl(v), curl(u))*dx            # Stiffness form
+#s = (1/mu0)*dot(curl(v), curl(u))*dx            # Stiffness form
+
+def boundary(x, on_boundary):
+    return on_boundary
+zero = dol.Expression(("0.0", "0.0", "0.0"), degree=1)
+bc = dol.DirichletBC(V, zero, boundary)
+
+
+
+# Assemble smass form
+M = dol.PETScMatrix()
+S = dol.PETScMatrix()
+dol.assemble(m, tensor=M, mesh=mesh)
+bc.apply(M)
+dol.assemble(s, tensor=S, mesh=mesh)
+bc.apply(S)
+#M, S = dol.assemble_system(m, s, bc)
+
+
+sigma = 2
+smat = S - sigma*M
+#lu = dol.LUSolver(S - sigma*M)
+lu = dol.LUSolver(smat)
+lu.parameters["reuse_factorization"] = True
+lu.parameters["report"] = False
+bb = dol.Vector(M.size(0))
+xx = dol.Vector(M.size(0))
+def sigma_solve(b):
+    bb[:] = b
+    lu.solve(xx, bb)
+    return xx[:]
+M_matvec = lambda x: M*x
+
+arpack_eigs,v = speigs.ARPACK_gen_eigs(M_matvec, sigma_solve, M.size(0), sigma, 51, ncv=91)
+
+# Create eigensolver
+# esolver = dol.SLEPcEigenSolver(S,M)
+# esolver.parameters["spectrum"] = "smallest real"
+# esolver.parameters["solver"] = "arnoldi"
+# esolver.parameters["spectral_shift"] = sigma
+# esolver.parameters["spectral_transform"] = "shift-and-invert"
+# esolver.solve()
+
+# eigs = [esolver.get_eigenvalue(i)[0] for i in range(4)]
+# filtered_eigs = []
+# for i in range(S.size(0)):
+#     ev_r, ev_i = esolver.get_eigenvalue(i)
+#     if ev_r > 0.001:
+#         filtered_eigs.append(ev_r)
+
+res = N.array(sorted(arpack_eigs)[0:10])
+print N.sqrt(res)
+#errs = postproc_eigres.calc_errs(res)
+#postproc_eigres.print_errs(errs)
+
+
---
+++ fenics_test/postproc_eigres.py
@@ -0,0 +1,24 @@
+from __future__ import division
+
+import numpy as N
+from postproc_code.AnalyticResults import PEC_cavity, err_percentage
+from NewCode import Utilities
+
+class EigenErrors(object):
+    pass
+
+def calc_errs(eigenvalues, geomname='rect-white', RMS_modes=4):
+    errs = EigenErrors()
+    errs.eigenvalues = eigenvalues
+    errs.geomname = geomname
+    errs.RMS_modes = RMS_modes 
+    errs.ares = PEC_cavity[geomname]
+    errs.eigenvalue_errors = err_percentage(errs.ares, eigenvalues)
+    errs.RMS_eigenvalue_errors = Utilities.RMS(errs.eigenvalue_errors[0:4])
+    return errs
+
+def print_errs(errs):
+    print 'eigenvalues: \n', errs.eigenvalues
+    print 'RMS error (%%) over first %d modes: %s' % (
+        errs.RMS_modes, str(errs.RMS_eigenvalue_errors))
+    print 'mode errors (%): \n', errs.eigenvalue_errors
