ec0e2e1fee6bfbf2ca74781f007225a4fa00a329
Neilen Marais <nmarais@gmail.com>
2011-05-13 20:13:42 +0200

    Update infinitesimal test to use spherical mesh and better plotting

--- examples/infinitesimal_dipole/test.py
+++ examples/infinitesimal_dipole/test.py
@@ -12,8 +12,8 @@ from FenicsCode.Consts import eps0, mu0, c0
 from FenicsCode.Utilities.MeshIO import femmesh_2_dolfin_mesh
 import dolfin as dol
 
-order = 1
-source_coord = N.array([0,0,0], N.float64) + 1e-5
+order = 2
+source_coord = N.array([0,0,1e-5]) 
 source_pt = dol.Point(*source_coord)
 
 parameters = dict(test_data.problem_data)
@@ -28,16 +28,22 @@ lam = c0/test_data.problem_data['f']
 # #domain_subdivisions = N.array([21,21,21], N.uint)
 # parameters.update(dict(domain_size=domain_size,
 #                        domain_subdivisions=domain_subdivisions))
+
+
 mesh_file = '../../workspace/sphere-r1m-6.femmesh'
 mesh = femmesh_2_dolfin_mesh(mesh_file)
 mesh.init()
+# mesh = dol.UnitCube(3,3,3)
+# mesh.coordinates()[:] -= 0.5
+# mesh.coordinates()[:] *= lam
 workspace['mesh'] = mesh
 
 parameters.update(dict(source_coord=source_coord,
                        order=order,
-                       solver='direct'))
+                       solver='iterative'))
 driver.run(parameters, workspace)
-E_1 = driver.get_E_field(workspace, test_data.r_1)
+last = 18
+E_1 = driver.get_E_field(workspace, test_data.r_1[0:last+1])
 #E_2 = driver.get_E_field(workspace, test_data.r_2)
 
 V = workspace['V']
@@ -46,24 +52,24 @@ x = workspace['x']
 A = workspace['A']
 b = workspace['b']
 
-u_abs = dol.Function(V)
-u_re = dol.Function(V)
-u_im = dol.Function(V)
-u_abs.vector()[:] = N.abs(x)
-u_re.vector()[:] = N.real(x)
-u_im.vector()[:] = N.imag(x)
-#dol.plot(u, interactive=True, axes=True)
-#dol.plot(mesh, interactive=True, axes=True)
-
-last = 18
-ratio = N.abs(E_1[last] / test_data.E_1[last])
-
-u_abs_recons = N.array([u_abs(r) for r in test_data.r_1[0:last+1]])
-u_re_recons = N.array([u_re(r) for r in test_data.r_1[0:last+1]])
-u_im_recons = N.array([u_im(r) for r in test_data.r_1[0:last+1]])
+M = workspace['M']
+S = workspace['S']
+S_0 = workspace['S_0']
 
 
-
-E_1_norm = E_1[0:last+1]/ratio
-
-
+from pylab import *
+r1 = test_data.r_1[1:last+1]/lam
+x1 = r1[:,0]
+E_1_ana = test_data.E_1[1:last+1]
+E_1_num = E_1[1:last+1]
+figure(1)
+plot(x1, N.abs(E_1_num[:,0]), '-g', label='x_num')
+plot(x1, N.abs(E_1_num[:,1]), '-b', label='y_num')
+plot(x1, N.abs(E_1_num[:,2]), '-r', label='z_num')
+plot(x1, N.abs(E_1_ana[:,0]), '--g', label='x_ana')
+plot(x1, N.abs(E_1_ana[:,1]), '--b', label='y_ana')
+plot(x1, N.abs(E_1_ana[:,2]), '--r', label='z_ana')
+ylabel('E-field Magnitude')
+xlabel('Distance (wavelengths)')
+legend(loc='best')
+show()
