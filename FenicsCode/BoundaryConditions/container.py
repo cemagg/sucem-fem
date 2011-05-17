63386f63eeaed034ddb693ada296edf222adde0a
Neilen Marais <nmarais@gmail.com>
2011-05-17 20:43:12 +0200

    Begin work to include ABC and driven problem into FenicsCode

---
+++ FenicsCode/BoundaryConditions/container.py
@@ -0,0 +1,41 @@
+from __future__ import division
+from FenicsCode import Forms
+
+
+class BoundaryConditions(object):
+    def __init__(self):
+        self.boundary_conditions = {}
+        
+    def add_boundary_condition(self, boundary_condition):
+        bc_num = boundary_condition.region_number
+        # boundary condition numbers have to be unique
+        assert(bc_num not in self.boundary_conditions)
+        self.boundary_conditions[bc_num] = boundary_condition
+
+    def apply_essential(self, A, b=None):
+        """
+        Apply essential boundary conditions to system matrix A and optional RHS b
+        """
+        for bc_num, bc in self.boundary_conditions.items():
+            apply_fn = bc.get_essential_application_func()
+            if b: apply_fn(A, b)
+            else: apply_fn(A)
+
+    def get_linear_form(self):
+        """Get boundary conditions contribution to RHS linear form
+        """
+        lin_form = Forms.NullForm()
+        for bc_num, bc in self.boundary_conditions.items():
+            lin_form = lin_form + bc.get_linear_form()
+
+        return lin_form
+
+    def get_bilinear_form(self):
+        """Get boundary conditions contribution to bilinear form
+        """
+
+        bilin_form = Forms.NullForm()
+        for bc_num, bc in self.boundary_conditions.items():
+            bilin_form = bilin_form + bc.get_bilinear_form()
+
+        return bilin_form
---
+++ examples/infinitesimal_dipole/new_driver.py
@@ -0,0 +1,37 @@
+from __future__ import division
+
+import sys
+sys.path.append('../../')
+import numpy as N
+import os
+import dolfin
+from FenicsCode.Consts import eps0, mu0, c0
+from FenicsCode.ProblemConfigurations.EMDrivenProblem import DrivenProblemABC
+import setup_mesh
+sys.path.insert(0, '../../')
+from test_data import problem_data
+del sys.path[0]
+
+## Problem parameters
+I = problem_data['I']                   # Dipole current
+l = problem_data['l']                   # Dipole length
+source_value = N.array([0,0,1.])*I*l
+freq = problem_data['f']
+lam = c0/freq
+source_coord = N.array([0,0,0.]) 
+## Discretisation settings
+order = 2
+domain_size = N.array([2*lam]*3)
+max_edge_len = lam/6
+mesh = setup_mesh.get_centred_cube(domain_size, max_edge_len, source_coord)
+
+## Implementation
+material_mesh_func = dolfin.MeshFunction('uint', mesh, 3)
+material_mesh_func.set_all(0)
+materials = {0:dict(eps_r=1, mu_r=1),}
+dp = DrivenProblemABC()
+dp.set_mesh(mesh)
+dp.set_basis_order(order)
+dp.set_material_regions(materials)
+dp.set_region_meshfunction(material_mesh_func)
+dp.init_problem()
