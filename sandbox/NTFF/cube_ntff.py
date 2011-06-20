9b05781f86521547d7789e4c38b26bfdc5483883
Neilen Marais <nmarais@gmail.com>
2011-06-20 18:30:31 +0200

    Modify ntff driver for timing changes

--- sandbox/NTFF/cube_ntff.py
+++ sandbox/NTFF/cube_ntff.py
@@ -17,7 +17,8 @@ fname = 'reference_dofs-2-0.149896229-0.0499654096667.pickle'
 #fname = 'dofs-2-0.599584916-0.0499654096667.pickle'
 #fname = 'dofs-2-1.199169832-0.0499654096667.pickle'
 #fname = 'interpdofs-2-0.599584916-0.0499654096667.pickle'
-theta_deg = N.array([0, 13, 79, 90, 140, 179])
+theta_deg = N.linspace(0, 180, 181)
+#theta_deg = N.array([0, 13, 79, 90, 140, 179])
 no_ff_pts = len(theta_deg)
 phi_deg = N.linspace(0, 360, no_ff_pts)
 #phi_deg = N.zeros(91)
@@ -37,14 +38,14 @@ E_phi = E_ff[:,1]
 E_theta_an = Z0*N.exp(-1j*k0)*(1j*k0*1*lam/1000)/(4*N.pi)*N.sin(N.deg2rad(theta_deg))
 ff_result_data = dict(E_ff=E_ff, E_theta_analytical=E_theta_an,
                       theta_deg=theta_deg, phi_deg=phi_deg)
-pickle.dump(dict(ff_result_data=ff_result_data, nf_input_data=data),
-            open(fname.replace('dofs', 'surface_ntff'), 'w'))
-import pylab 
-pylab.figure()
-pylab.plot(theta_deg, N.abs(E_theta), label='|E_theta|')
-pylab.plot(theta_deg, N.abs(E_phi), label='|E_phi|')
-pylab.plot(theta_deg, N.abs(E_theta_an), label='analytical')
-pylab.legend()
-pylab.grid(1)
-pylab.show()
+# pickle.dump(dict(ff_result_data=ff_result_data, nf_input_data=data),
+#             open(fname.replace('dofs', 'surface_ntff'), 'w'))
+# import pylab 
+# pylab.figure()
+# pylab.plot(theta_deg, N.abs(E_theta), label='|E_theta|')
+# pylab.plot(theta_deg, N.abs(E_phi), label='|E_phi|')
+# pylab.plot(theta_deg, N.abs(E_theta_an), label='analytical')
+# pylab.legend()
+# pylab.grid(1)
+# pylab.show()
 
