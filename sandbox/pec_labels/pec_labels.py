32691beb1645c8160be95cefda9ffde1cbc98121
Evan Lezar <mail@evanlezar.com>
2011-06-22 16:16:21 +0900

    Added a generalized version of gmsh converter. Now to try to contruct mesh Object on the fly.

--- sandbox/pec_labels/pec_labels.py
+++ sandbox/pec_labels/pec_labels.py
@@ -5,6 +5,222 @@ from dolfin import *
 from dolfin_utils import meshconvert
 import numpy as np
 import os
+
+def gmsh_to_dolfin_mesh ( ifilename, handler ):
+    """Convert between .gmsh v2.0 format (http://www.geuz.org/gmsh/) and .xml,
+    parser implemented as a state machine:
+
+        0 = read 'MeshFormat'
+        1 = read  mesh format data
+        2 = read 'EndMeshFormat'
+        3 = read 'Nodes'
+        4 = read  number of vertices
+        5 = read  vertices
+        6 = read 'EndNodes'
+        7 = read 'Elements'
+        8 = read  number of cells
+        9 = read  cells
+        10 = done
+
+    Afterwards, extract physical region numbers if they are defined in
+    the mesh file as a mesh function.
+
+    """
+
+    print "Converting from Gmsh format (.msh, .gmsh) to DOLFIN XML format"
+    
+    # The dimension of the gmsh element types supported here as well as the dolfin cell types for each dimension 
+    gmsh_dim = {1: 1, 2: 2, 4: 3}
+    gmsh_cell_type = {1: "interval", 2: "triangle", 3: "tetrahedron" }
+    # the gmsh element types supported for conversion
+    supported_gmsh_element_types = [1, 2, 4]
+    
+    # Open files
+    ifile = open(ifilename, "r")
+
+    # Scan file for cell type
+    cell_type = None
+    highest_dim = 0
+    line = ifile.readline()
+    while line:
+
+        # Remove newline
+        if line[-1] == "\n":
+            line = line[:-1]
+
+        # Read dimension
+        if line.find("$Elements") == 0:
+
+            line = ifile.readline()
+            num_cells  = int(line)
+            num_cells_counted = 0
+            if num_cells == 0:
+                _error("No cells found in gmsh file.")
+            line = ifile.readline()
+
+            # Now iterate through elements to find largest dimension.  Gmsh
+            # format might include elements of lower dimensions in the element list.
+            # We also need to count number of elements of correct dimensions.
+            # Also determine which vertices are not used.
+            dim_count = {1: 0, 2: 0, 3: 0}
+            vertices_used = {1: [], 2: [], 3: []}
+            # Array used to store gmsh tags for 1D (type 1/line), 2D (type 2/triangular) elements and 3D (type 4/tet) elements
+            tags_for_dim = {1: [], 2: [], 3: []}
+            
+            while line.find("$EndElements") == -1:
+                element = line.split()
+                elem_type = int(element[1])
+                num_tags = int(element[2])
+                
+                if elem_type in supported_gmsh_element_types:
+                    dim = gmsh_dim[elem_type]
+                    if highest_dim < dim:
+                        highest_dim = dim
+                    
+                    node_num_list = [int(node) for node in element[3 + num_tags:]]
+                    vertices_used[dim].extend(node_num_list)
+                    if num_tags > 0:
+                        tags_for_dim[dim].append(tuple(int(tag) for tag in element[3:3+num_tags]))    
+                    dim_count[dim] += 1
+                else:
+                    #TODO: output a warning here. "gmsh element type %d not supported" % elem_type
+                    pass
+
+                line = ifile.readline()
+        else:
+            # Read next line
+            line = ifile.readline()
+
+    # Check that we got the cell type and set num_cells_counted
+    if highest_dim == 0:
+        _error("Unable to find cells of supported type.")
+    
+    num_cells_counted = dim_count[highest_dim]
+    vertex_set = set(vertices_used[highest_dim])
+    vertices_used[highest_dim] = None
+
+    vertex_dict = {}
+    for n,v in enumerate(vertex_set):
+        vertex_dict[v] = n
+
+    # Step to beginning of file
+    ifile.seek(0)
+    
+    # Set mesh type
+    handler.set_mesh_type(gmsh_cell_type[highest_dim], highest_dim)
+
+    # Initialise node list (gmsh does not export all vertexes in order)
+    nodelist = {}
+
+    # Current state
+    state = 0
+
+    # Write data
+    num_vertices_read = 0
+    num_cells_read = 0
+
+    while state != 10:
+
+        # Read next line
+        line = ifile.readline()
+        if not line: break
+
+        # Skip comments
+        if line[0] == '#':
+            continue
+
+        # Remove newline
+        if line[-1] == "\n":
+            line = line[:-1]
+
+        if state == 0:
+            if line == "$MeshFormat":
+                state = 1
+        elif state == 1:
+            (version, file_type, data_size) = line.split()
+            state = 2
+        elif state == 2:
+            if line == "$EndMeshFormat":
+                state = 3
+        elif state == 3:
+            if line == "$Nodes":
+                state = 4
+        elif state == 4:
+            num_vertices = len(vertex_dict)
+            handler.start_vertices(num_vertices)
+            state = 5
+        elif state == 5:
+            (node_no, x, y, z) = line.split()
+            node_no = int(node_no)
+            x,y,z = [float(xx) for xx in (x,y,z)]
+            if vertex_dict.has_key(node_no):
+                node_no = vertex_dict[node_no]
+            else:
+                continue
+            nodelist[int(node_no)] = num_vertices_read
+            handler.add_vertex(num_vertices_read, [x, y, z])
+            num_vertices_read +=1
+
+            if num_vertices == num_vertices_read:
+                handler.end_vertices()
+                state = 6
+        elif state == 6:
+            if line == "$EndNodes":
+                state = 7
+        elif state == 7:
+            if line == "$Elements":
+                state = 8
+        elif state == 8:
+            handler.start_cells(num_cells_counted)
+            state = 9
+        elif state == 9:
+            element = line.split()
+            elem_type = int(element[1])
+            num_tags  = int(element[2])
+            if elem_type in supported_gmsh_element_types:
+                dim = gmsh_dim[elem_type]
+            else:
+                dim = 0
+            if dim == highest_dim:
+                node_num_list = [vertex_dict[int(node)] for node in element[3 + num_tags:]]
+                node_num_list.sort()
+                for node in node_num_list:
+                    if not node in nodelist:
+                        _error("Vertex %d of %s %d not previously defined." %
+                              (node, gmsh_cell_type[dim], num_cells_read))
+                cell_nodes = [nodelist[n] for n in node_num_list]
+                handler.add_cell(num_cells_read, cell_nodes)
+                num_cells_read +=1
+
+            if num_cells_counted == num_cells_read:
+                handler.end_cells()
+                state = 10
+        elif state == 10:
+            break
+
+    # Write mesh function based on the Physical Regions defined by
+    # gmsh, but only if they are not all zero. All zero physical
+    # regions indicate that no physical regions were defined.
+    if highest_dim not in [1,2,3]:
+        _error("Gmsh tags not supported for dimension %i. Probably a bug" % dim)
+        
+    tags = tags_for_dim[highest_dim]
+    physical_regions = tuple(tag[0] for tag in tags)
+    if not all(tag == 0 for tag in tags):
+        handler.start_meshfunction("physical_region", dim, num_cells)
+        for i, physical_region in enumerate(physical_regions):
+            handler.add_entity_meshfunction(i, physical_region)
+        handler.end_meshfunction()
+    
+    # Check that we got all data
+    if state == 10:
+        print "Conversion done"
+    else:
+       _error("Missing data, unable to convert \n\ Did you use version 2.0 of the gmsh file format?")
+
+    # Close files
+    ifile.close()
+
         
 def process_gmsh_elements ( ifilename, dim ):
     """
@@ -69,6 +285,9 @@ def convert_and_create_facet_mesh_function ( ifilename, ofilename ):
     # Now load the created mesh and initialise the required connectivity information
     mesh = Mesh ( ofilename )
     mesh.order()
+    
+    File ( ofilename ) << mesh
+    
     D = mesh.topology().dim()
     mesh.init(D-1, 0)
     
@@ -107,6 +326,15 @@ def convert_and_create_facet_mesh_function ( ifilename, ofilename ):
     
     mesh_function_file << facet_mark_function
 
+
+def to_mesh ( ifile ):
+    me = meshconvert.XmlHandler( 'tmp.xml' )
+    
+    gmsh_to_dolfin_mesh( ifile, me )
+    
+    me.close()
+
 if __name__ == "__main__":
-    convert_and_create_facet_mesh_function ( "untitled.msh", "untitled.xml" )
+    to_mesh( "untitled.msh" )
+#    convert_and_create_facet_mesh_function ( "untitled.msh", "untitled.xml" )
     
