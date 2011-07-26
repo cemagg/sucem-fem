ea26c37e28b795b7687b45ee24c674727849f9bb
Neilen Marais <nmarais@gmail.com>
2011-07-26 15:36:11 +0200

    Fix (I think) CalcEMFunctional. At least it is symmetrical now

--- sandbox/flux_calc/flux_calc.py
+++ sandbox/flux_calc/flux_calc.py
@@ -1,3 +1,21 @@
+## Copyright (C) 2011 Stellenbosch University
+##
+## This file is part of SUCEM.
+##
+## SUCEM is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## SUCEM is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with SUCEM. If not, see <http://www.gnu.org/licenses/>. 
+##
+## Contact: cemagga@gmail.com 
 # Authors:
 # Neilen Marais <nmarais@gmail.com>
 
@@ -57,7 +75,7 @@ ReS = (1/k0/Z0)*dolfin.dot(n, (dolfin.cross(E_r, -dolfin.curl(E_i)) +
                                dolfin.cross(E_i, dolfin.curl(E_r))))*dolfin.ds
 energy_flux = dolfin.assemble(ReS)
 
-var_method = 'swapped2'
+var_method = 'original'
 
 def boundary(x, on_boundary):
     return on_boundary
